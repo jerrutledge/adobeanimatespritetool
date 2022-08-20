047f3902c9adadb83369595950713db457d27ba0
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2022-08-20 19:36:28 -0300

    expand crop automatically

--- process_gif.py
+++ process_gif.py
@@ -1,4 +1,6 @@
 import os, shutil
+from tabnanny import check
+from turtle import width
 from PIL import Image
 import cv2
 import numpy as np
@@ -17,30 +19,68 @@ def emptyAndDeleteFolder(foldername):
             raise RuntimeError
     os.rmdir(foldername)
 
-def processGif(input_filename, output_filename, x=0, y=0, w=1000, h=1000, replace = False):
-    # capture the animated gif
+def autoCrop(frames, g_x=0, g_y=0, g_w=1, g_h=1):
+    # expand from given points every frame until a completely transparent border is reached
+    x, y, w, h = g_x, g_y, g_w, g_h
+    
+    image_width, image_height = frames[0].shape[0], frames[0].shape[1]
+    dirs = [True, True, True, True] # keep expanding in up, down, left & right
+    while True in dirs:
+        dirs = [True, True, True, True]
+        dirs[0] = checkRowOrColumn(frames, x, x+w, y, y+1) and y > 1
+        y -= 1 if dirs[0] else 0
+        dirs[1] = checkRowOrColumn(frames, x, x+w, y+h-1, y+h) and h < image_height - y
+        h += 1 if dirs[1] else 0
+        dirs[2] = checkRowOrColumn(frames, x, x+1, y, y+h) and x > 1
+        x -= 1 if dirs[2] else 0
+        dirs[3] = checkRowOrColumn(frames, x+w-1, x+w, y, y+h) and w < image_width - x
+        w += 1 if dirs[3] else 0
+
+    return x, y, w, h
+
+def checkRowOrColumn(frames, x1, x2, y1, y2):
+    try:
+        for frame in frames:
+            if frame[x1:x2, y1:y2].max() >= 0:
+                # we've reached an non-blank pixel
+                return True
+    except IndexError:
+        print('HELP')
+        return False
+    # everything is transparent
+    return False
+
+
+def processGif(input_filename, output_filename, g_x=0, g_y=0, g_w=1000, g_h=1000, replace = False, auto_crop=True):
+    # capture the animated gif using PIL
     frames = []
     with Image.open(input_filename) as imageObject:
         for frame_num in range(0, imageObject.n_frames):
             imageObject.seek(frame_num)
             frames.append(cv2.cvtColor(np.array(imageObject), cv2.COLOR_BGR2RGBA))
 
+    # determine the correct crop starting from a given crop
+    x, y, w, h = g_x, g_y, g_w, g_h
+    if auto_crop:
+        x, y, w, h = autoCrop(frames, g_x, g_y, g_w, g_h)
+        print("Autocrop", x,y,w,h)
+
     #remove duplicate frames
     prevCropFrame = []
     uniqueFrames = []
     for frame in frames[1:]: # only the frames after the first 
         # crop
-        cropFrame = frame[y:y+h, x:x+w]
+        cropFrame = frame[x:x+w, y:y+h]
         if len(prevCropFrame):
             norm = cv2.norm(prevCropFrame, cropFrame)
-            print(norm / (w*h))
+            # print(norm / (w*h))
             if norm > 0.07 and cropFrame.max() > 0:
                 uniqueFrames.append(cropFrame)
         else:
             uniqueFrames.append(cropFrame)
         prevCropFrame = cropFrame
 
-    print(len(uniqueFrames))
+    print(len(uniqueFrames), "unique frames total")
     if os.path.isdir(output_filename):
         print("Directory", output_filename, "already exists")
         if replace:
@@ -55,7 +95,6 @@ def processGif(input_filename, output_filename, x=0, y=0, w=1000, h=1000, replac
             # output to directory called output filename
             file_name = output_filename + "/" + output_filename + "_" + str(i) + ".png"
             cv2.imwrite(file_name, uniqueFrames[i])
-            print(file_name)
         return
     except Exception as err:
         print("ERROR, write failed")
@@ -69,6 +108,6 @@ if __name__ == "__main__":
     input_filename = "Character-walk-cycle2.gif"
     output_filename = "Character"
     # crop
-    x, y, w, h = 1, 1, 460, 500
+    x, y, w, h = 50, 50, 60, 60
 
     processGif(input_filename, output_filename, x, y, w, h, replace=True)
